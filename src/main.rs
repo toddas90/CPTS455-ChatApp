7bf2be49e227955bd2fc61092cf5f1edeacd506c
Andrew Todd <andrew123ast@gmail.com>
2022-10-24 17:57:34 +0000

    Added some initial ideas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Intial thoughts:
+// I think this will be a monolithic application. It will act as a server
+// or a client, depending on the command line arguments.
+// Someone will create a server, and multiple clients can connect to it.
+// The server will be able to send messages to all clients, and clients
+// will be able to send messages to the server and to other clients.

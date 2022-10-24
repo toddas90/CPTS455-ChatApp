bafdf58b77a2dcf74c0dc054c67d6551bb27de7b
Andrew Todd <andrew123ast@gmail.com>
2022-10-24 20:47:59 +0000

    Changed main to return err, started server code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::error::Error;
 
 use clap::{arg, command, Parser};
 
@@ -9,18 +9,21 @@ pub mod user;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    // If the port is not specified, we'll use 8080.
     #[arg(short, long, default_value_t = 8080)]
     port: u16,
 
+    // If the host is not specified, we'll assume this user wants to create the server.
     #[arg(short, long)]
     server_address: Option<String>,
 
+    // If the username is not specified, we'll use "Anonymous".
     #[arg(short, long)]
     username: Option<String>,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     if args.server_address.is_none() {
@@ -28,10 +31,11 @@ async fn main() {
     } else {
         client(args).await;
     }
+    Ok(())
 }
 
 async fn server(args: Args) {
-    // ...
+    let addr = format!("127.0.0.1:{}", args.port);
 }
 
 async fn client(args: Args) {

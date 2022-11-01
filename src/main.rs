6c783fe318cef48ca4e4c29bce52ea48f652412e
Andrew Todd <andrew123ast@gmail.com>
2022-11-01 23:09:30 +0000

    Implemented basic file transfers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::{arg, command, Parser};
+use crossterm::{cursor::MoveUp, terminal::Clear, ExecutableCommand};
 use std::{error::Error, io};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::tcp::OwnedWriteHalf,
     sync::broadcast,
 };
 
@@ -78,39 +80,81 @@ async fn server(args: Args) -> Result<(), Box<dyn Error>> {
             let (reader, mut writer) = socket.split();
             let mut reader = tokio::io::BufReader::new(reader);
             let mut line = String::new();
+            let mut temp_file: message::FileMessage =
+                message::FileMessage::new(&dummy_user, "temp", 0, &vec![]);
 
             // Main loop.
             loop {
                 tokio::select! {
                     // Read a message from the client.
                     _ = reader.read_line(&mut line) => {
-                        println!("Received msg from {}", reader.get_ref().peer_addr().unwrap());
+                        println!("Received from {}", reader.get_ref().peer_addr().unwrap());
                         if line.is_empty() {
                             break;
                         }
 
-                        // If the message is a valid JSON, we'll send it to the channel.
-                        // If not, we wrap it in a message before sending it.
-                        if serde_json::from_str::<message::Message>(&line).is_ok() {
+                        if serde_json::from_str::<message::Command>(&line).is_ok() {
+                            let command: message::Command = serde_json::from_str(&line).unwrap();
+                            if command.command == "/recvinfo" {
+                                println!("Received command");
+                                let msg = format!("{}: {} bytes -> {}\n", temp_file.username, temp_file.file_size, temp_file.file_name);
+                                writer.write_all(msg.as_bytes()).await.unwrap();
+                                line.clear();
+                            }
+                            if command.command == "/recvfile" {
+                                println!("Received command");
+                                let mut serialized = serde_json::to_string(&temp_file).unwrap();
+                                serialized += "\n";
+                                writer.write_all(serialized.as_bytes()).await.unwrap();
+                                writer.write_all("Ok\n".to_string().as_bytes()).await.unwrap();
+                                line.clear();
+                            }
+                        } else if serde_json::from_str::<message::FileMessage>(&line).is_ok() {
+                            println!("Received file message");
+                            temp_file = serde_json::from_str(&line).unwrap();
+                            writer.write_all("Ok\n".to_string().as_bytes()).await.unwrap();
+
                             let message = line.clone();
                             tx.send(message).expect("Failed to send message");
                             line.clear();
-                        } else {
+                        } else if serde_json::from_str::<message::TextMessage>(&line).is_ok() {
+                            println!("Received text message");
+                            let message = line.clone();
+                            tx.send(message).expect("Failed to send message");
+                            line.clear();
+                        }  else {
                             println!("Message from Telnet client; Wrappping in message struct");
-                            let message = message::Message::new(&dummy_user, &line, chrono::Utc::now());
+                            let message = message::TextMessage::new(&dummy_user, &line, chrono::Utc::now());
                             let message = serde_json::to_string(&message).unwrap();
                             tx.send(message).expect("Failed to send message");
                             line.clear();
                         }
+
+                        // // If the message is a valid JSON, we'll send it to the channel.
+                        // // If not, we wrap it in a message before sending it.
+                        // // (This is to handle telnet clients)
+                        // if serde_json::from_str::<message::TextMessage>(&line).is_ok() {
+                        //     let message = line.clone();
+                        //     tx.send(message).expect("Failed to send message");
+                        //     line.clear();
+                        // } else {
+                        //     println!("Message from Telnet client; Wrappping in message struct");
+                        //     let message = message::TextMessage::new(&dummy_user, &line, chrono::Utc::now());
+                        //     let message = serde_json::to_string(&message).unwrap();
+                        //     tx.send(message).expect("Failed to send message");
+                        //     line.clear();
+                        // }
                     }
                     // Send the message to the other clients.
                     result = rx.recv() => {
                         let message = result.unwrap();
                         // If the message is a valid JSON, we'll send it to the client.
-                        if serde_json::from_str::<message::Message>(&message).is_ok() {
-                            let message = serde_json::from_str::<message::Message>(&message).unwrap();
-                            let message = format!("{} {}: {}", message.created_at.timestamp(), message.username, message.body);
+                        if serde_json::from_str::<message::TextMessage>(&message).is_ok() {
+                            let message = serde_json::from_str::<message::TextMessage>(&message).unwrap();
+                            let message = format!("{} {}: {}", message.created_at.format("%d/%m/%Y %H:%M"), message.username, message.body);
                             writer.write_all(message.as_bytes()).await.unwrap();
+                        } else if serde_json::from_str::<message::FileMessage>(&message).is_ok() {
+                            temp_file = serde_json::from_str::<message::FileMessage>(&message).unwrap();
                         } else {
                             writer.write_all(message.as_bytes()).await.unwrap();
                         }
@@ -144,7 +188,16 @@ async fn client(args: Args) -> Result<(), Box<dyn Error>> {
         loop {
             // Read a line from the server.
             let line = reader.next_line().await.unwrap().unwrap();
-            println!("{}", line);
+
+            if serde_json::from_str::<message::FileMessage>(&line).is_ok() {
+                let file_message = serde_json::from_str::<message::FileMessage>(&line).unwrap();
+                let mut file = tokio::fs::File::create(file_message.file_name)
+                    .await
+                    .unwrap();
+                file.write_all(&file_message.file_data).await.unwrap();
+            } else {
+                println!("{}", line);
+            }
         }
     });
 
@@ -152,10 +205,85 @@ async fn client(args: Args) -> Result<(), Box<dyn Error>> {
     loop {
         // Read a line from the user and send it to the server.
         io::stdin().read_line(&mut line).unwrap();
-        let message = message::Message::new(&user_info, &line, chrono::Utc::now());
-        let mut message = serde_json::to_string(&message).unwrap();
-        message += "\n";
-        writer.write_all(message.as_bytes()).await.unwrap();
-        line.clear();
+
+        if line.starts_with('/') {
+            match process_cmd(&line, &mut writer, &user_info).await {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+            line.clear();
+        } else {
+            // Console commands to clear the user's input.
+            io::stdout().execute(crossterm::cursor::MoveUp(1)).unwrap();
+            io::stdout()
+                .execute(crossterm::cursor::MoveToColumn(0))
+                .unwrap();
+            io::stdout()
+                .execute(crossterm::terminal::Clear(
+                    crossterm::terminal::ClearType::CurrentLine,
+                ))
+                .unwrap();
+            // ------------------------------
+            let message = message::TextMessage::new(&user_info, &line, chrono::Utc::now());
+            let mut message = serde_json::to_string(&message).unwrap();
+            message += "\n";
+            writer.write_all(message.as_bytes()).await.unwrap();
+            line.clear();
+        }
+    }
+}
+
+async fn process_cmd(
+    cmd: &str,
+    writer: &mut OwnedWriteHalf,
+    user_info: &user::User,
+) -> Result<(), Box<dyn Error>> {
+    let cmd = cmd.trim();
+    let cmd = cmd.split_whitespace().collect::<Vec<&str>>();
+    match cmd[0] {
+        "/help" => {
+            println!("Available commands:");
+            println!("    /help - Show this message");
+            println!("    /send - Send a file");
+            println!("    /recvinfo - Information about the file");
+            println!("    /recv - Receive a file");
+            println!("    /quit - Quit the application");
+            Ok(())
+        }
+        "/send" => {
+            let file_name = cmd[1].split('/').last().unwrap();
+
+            let file = match std::fs::read(cmd[1]) {
+                Ok(file) => file,
+                Err(e) => {
+                    return Err(Box::new(e));
+                }
+            };
+            let message = message::FileMessage::new(user_info, file_name, file.len(), &file);
+            let mut message = serde_json::to_string(&message).unwrap();
+            message += "\n";
+            writer.write_all(message.as_bytes()).await.unwrap();
+            Ok(())
+        }
+        "/recvinfo" => {
+            let cmd = message::Command::new(user_info, cmd[0]);
+            let mut message = serde_json::to_string(&cmd).unwrap();
+            message += "\n";
+            writer.write_all(message.as_bytes()).await.unwrap();
+            Ok(())
+        }
+        "/recvfile" => {
+            let cmd = message::Command::new(user_info, cmd[0]);
+            let mut message = serde_json::to_string(&cmd).unwrap();
+            message += "\n";
+            writer.write_all(message.as_bytes()).await.unwrap();
+            Ok(())
+        }
+        "/quit" => {
+            std::process::exit(0);
+        }
+        _ => Err("Unknown command".into()),
     }
 }
--- src/message.rs
+++ src/message.rs
@@ -5,26 +5,64 @@ use uuid::Uuid;
 use crate::user;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Message {
+pub struct TextMessage {
     pub username: String,
     pub user_id: Uuid,
     pub body: String,
     pub created_at: DateTime<Utc>,
 }
 
-impl std::fmt::Display for Message {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}", self.username, self.body)
-    }
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct FileMessage {
+    pub username: String,
+    pub user_id: Uuid,
+    pub file_name: String,
+    pub file_size: usize,
+    pub file_data: Vec<u8>,
 }
 
-impl Message {
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Command {
+    pub username: String,
+    pub user_id: Uuid,
+    pub command: String,
+}
+
+// impl std::fmt::Display for TextMessage {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{}: {}", self.username, self.body)
+//     }
+// }
+
+impl TextMessage {
     pub fn new(user: &user::User, body: &str, created_at: DateTime<Utc>) -> Self {
-        Message {
+        TextMessage {
             username: user.username.to_owned(),
             user_id: user.user_id,
             body: String::from(body),
             created_at,
+        }
+    }
+}
+
+impl FileMessage {
+    pub fn new(user: &user::User, file_name: &str, file_size: usize, file_data: &Vec<u8>) -> Self {
+        FileMessage {
+            username: user.username.to_owned(),
+            user_id: user.user_id,
+            file_name: String::from(file_name),
+            file_size,
+            file_data: file_data.to_owned(),
+        }
+    }
+}
+
+impl Command {
+    pub fn new(user: &user::User, command: &str) -> Self {
+        Command {
+            username: user.username.to_owned(),
+            user_id: user.user_id,
+            command: String::from(command),
         }
     }
 }
